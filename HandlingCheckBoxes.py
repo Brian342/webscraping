9909392f0f071d5516951ba98d1cd83c4b95fad3
Brian kimanzi <migelbrian3@gmail.com>
2025-01-30 10:28:26 +0300

    selenium on a browser page performing, check-box on a single checkbox and multiple checkboxes, also counts the number of checkboxes and returns a specific condition

--- HandlingCheckBoxes.py
+++ HandlingCheckBoxes.py
@@ -1,4 +1,6 @@
 import time
+import tkinter as tk
+from tkinter import simpledialog
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,6 +11,13 @@ driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407")
 driver.maximize_window()
 
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # used to scroll the page of the site
+
+Root = tk.Tk()
+Root.withdraw()
+
+expected_checked_count = simpledialog.askinteger(title="CheckBox Count",
+                                                 prompt="Enter the expected_checked_count: ")
+print("expected_checked_count: ", expected_checked_count)
 
 checkboxes = driver.find_elements(By.XPATH, '//input[@type="checkbox"]')
 # iterates through the checkboxes
@@ -21,15 +30,12 @@ for checkbox in checkboxes:
     if checkbox.is_selected():
         checked_count += 1
 
-expected_checked_count = 7
-
 if checked_count == expected_checked_count:
     print('Checkbox count verified')
 else:
     print('Checkbox count mismatch')
 time.sleep(5)
 driver.close()
-
 
 # driver.find_element(By.XPATH, "//label[normalize-space()='Sunday']").click()  # checks the first checkbox
 
